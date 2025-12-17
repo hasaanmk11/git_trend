63790fa470eeb68f94ab2720c0e9eb6a8591ca6f
hasan mk <hasanmkxxx@gmail.com>
2025-12-17 10:26:48 +0530

    added provider basic functions

---
+++ app/lib/features/presentation/provider/repo_provider.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../../domain/entities/repo.dart';
+import '../../domain/usecases/fetch_trending_repos.dart';
+
+class RepoProvider extends ChangeNotifier {
+  final FetchTrendingRepos fetchTrendingRepos;
+
+  RepoProvider(this.fetchTrendingRepos);
+
+  List<Repo> repos = [];
+  bool isLoading = false;
+  bool isMoreLoading = false;
+  int currentPage = 1;
+  bool hasMore = true;
+
+  Future<void> loadRepos() async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      currentPage = 1;
+      final result = await fetchTrendingRepos(currentPage);
+      repos = result;
+      hasMore = result.length == 30;
+    } catch (e) {
+      print("Error loading repos: $e");
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> loadMore() async {
+    if (isMoreLoading || !hasMore) return;
+
+    isMoreLoading = true;
+    notifyListeners();
+
+    try {
+      currentPage++;
+      final result = await fetchTrendingRepos(currentPage);
+      repos.addAll(result);
+      hasMore = result.length == 30;
+    } catch (e) {
+      print("Error loading more repos: $e");
+    }
+
+    isMoreLoading = false;
+    notifyListeners();
+  }
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,22 @@
+import 'package:app/features/data/datasources/data_source.dart';
+import 'package:app/features/data/repositories/git_repo_imp.dart';
+import 'package:app/features/domain/usecases/fetch_trending_repos.dart';
 import 'package:app/features/presentation/app.dart';
+import 'package:app/features/presentation/provider/repo_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  final remote = GithubRemoteDatasource();
+  final repoImpl = GithubRepositoryImpl(remote);
+  final fetchUsecase = FetchTrendingRepos(repoImpl);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RepoProvider(fetchUsecase)),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
