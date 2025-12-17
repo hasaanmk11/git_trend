568f487315b169238da780e06a31f0186af76e8b
hasan mk <hasanmkxxx@gmail.com>
2025-12-17 12:42:34 +0530

    added more repos loading functions,now its loads more data when user scroll close to the bottom,used scroll controller

---
+++ app/lib/features/core/const/app_scroll_ctrl.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppScrollCtrl {
+  static final ScrollController scrollController = ScrollController();
+}
--- app/lib/features/presentation/pages/home_page.dart
+++ app/lib/features/presentation/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app/features/core/const/app_scroll_ctrl.dart';
 import 'package:app/features/presentation/provider/repo_provider.dart';
 import 'package:app/features/presentation/widgets/repos_crad.dart';
 import 'package:flutter/material.dart';
@@ -13,11 +14,20 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  @override
   void initState() {
     super.initState();
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<RepoProvider>().loadRepos();
+    });
+
+    AppScrollCtrl.scrollController.addListener(() {
+      final provider = context.read<RepoProvider>();
+
+      if (AppScrollCtrl.scrollController.position.pixels >=
+          AppScrollCtrl.scrollController.position.maxScrollExtent - 200) {
+        provider.loadMore();
+      }
     });
   }
 
@@ -28,12 +38,22 @@ class _HomePageState extends State<HomePage> {
       body: Consumer<RepoProvider>(
         builder: (context, value, child) {
           if (value.isLoading) {
-            return CircularProgressIndicator();
+            return Center(child: CircularProgressIndicator());
           }
           return ListView.builder(
-            itemCount: value.repos.length,
-            itemBuilder: (_, i) {
-              final repo = value.repos[i];
+            controller: AppScrollCtrl.scrollController,
+            itemCount: value.repos.length + 1,
+            itemBuilder: (_, index) {
+              if (index == value.repos.length) {
+                return value.isMoreLoading
+                    ? const Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Center(child: CircularProgressIndicator()),
+                      )
+                    : const SizedBox();
+              }
+
+              final repo = value.repos[index];
               return repoCard(
                 image: repo.owner.avatarUrl,
                 name: repo.name,
