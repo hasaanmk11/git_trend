ccb8c45af9f245616018f84fb3ddc4060203d71a
hasan mk <hasanmkxxx@gmail.com>
2025-12-17 10:10:29 +0530

    added the top text

---
+++ app/lib/features/core/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color appBlackColor = Colors.black;
+  static Color appWhiteColor = Colors.white;
+  static Color appGreyColor = Colors.grey;
+}
--- app/lib/features/presentation/pages/home_page.dart
+++ app/lib/features/presentation/pages/home_page.dart
@@ -1,11 +1,26 @@
-import 'package:app/features/core/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:app/features/core/widgets/app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppBar(), backgroundColor: Colors.white);
+    return Scaffold(
+      appBar: buildAppBar(),
+      backgroundColor: const Color(0xffF2F4F7),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Top starred repositories created in the last 60 days.',
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
